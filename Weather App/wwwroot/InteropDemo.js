0f89c0910bcc0f7b1bf3aea7aa655952ffc763b4
Vega <2187815@cognizant.com>
2023-08-28 08:14:00 -0300

    First commit.

---
+++ Weather App/wwwroot/InteropDemo.js
@@ -0,0 +1,15 @@
+﻿window.copyToClipboard = str => {
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+        return navigator.clipboard.writeText(str);
+    return Promise.reject('The Clipboard API is not available.');
+};
+
+
+window.showError = function(tittle, errorMessage){
+    Swal.fire({
+        icon: 'error',
+        tittle: tittle,
+        text: errorMessage,
+        footer: '<a href="mailto:demo@demo.com">Contact us if the problem persists</a>'
+    });
+};
